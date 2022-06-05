865a9864317bd6c16cc8b4a8ab6acd4405da5ce3
Alan Sampson <alansam@users.noreply.github.com>
2022-06-05 10:10:35 +0000

    Move passenger class implementation to it’s own file.

--- .ccls-cache/@home@runner@ItchyShorttermMotion/main.cpp
+++ .ccls-cache/@home@runner@ItchyShorttermMotion/main.cpp
@@ -13,24 +13,6 @@ initialize as given member value
 
 using namespace std::literals::string_literals;
 
-//  MARK: - Class passenger Implementation
-// copy constructor
-passenger::passenger(const passenger & pass) {
-    name = pass.name;
-    hve_ticket = pass.hve_ticket;
-    ticketname = pass.ticketname;
-    working = pass.working;
-}
-
-// default constructor
-// constructor given member values
-passenger::passenger(std::string const s_name,
-            std::string const s_ticket,
-            part const n_work)
-  : name { s_name },
-    ticketname { s_ticket },
-    working { n_work },
-    hve_ticket { (s_ticket != "NONE"s) } {}
 
 //  MARK: - Driver
 /*
